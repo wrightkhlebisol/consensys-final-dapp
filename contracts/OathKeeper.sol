eb2db3e76ca7a353297787117e497f16c8375310
wrightkhlebisol <calebogundiya@gmail.com>
2020-11-30 13:46:21 +0100

    Add modifers to relevant sessions and to ensure valid and access

--- contracts/OathKeeper.sol
+++ contracts/OathKeeper.sol
@@ -8,26 +8,26 @@ pragma solidity >=0.4.0 <0.8.0;
 
 contract OathKeeper{
     uint private oathCount = 0;
-    
+
     mapping (address => uint) public pendingWithdrawals;
-    
+
     mapping(uint => Oath) public oaths;
-    
+
     struct Oath {
         uint Id;
         string body;
         uint timeCreated;
         uint deadline;
         uint oathValue;
-        address oathGiver;
-        address oathTaker;
+        address payable oathGiver;
+        address payable oathTaker;
         address payable defaultsRecipient;
         address payable completionRecipient;
         bool useVerifiers;
     }
-    
+
     mapping(uint => Milestone[]) public milestones;
-    
+
     struct Milestone {
         // oathId acts as foreignkey to link milestone to oath
         string milestoneBody;
@@ -40,110 +40,118 @@ contract OathKeeper{
         bool oathTakerVerified;
         bool completed;
     }
-    
+
     /**
      * MODIFIERS
     */
-    modifier onlyOathTaker(){
+
+    modifier onlyOathGiver(address _oathGiver, uint _oathId){
+        require(_oathGiver == oaths[_oathId].oathGiver, "Access restricted to only Oath-Giver");
         _;
     }
-    
-    modifier onlyOathGiver(){
+
+    modifier onlyOathTaker(address _oathTaker, uint _oathId){
+        require(_oathTaker == oaths[_oathId].oathTaker, "Access restricted to only Oath-Taker");
         _;
     }
-    
-    modifier oathExists(){
+
+    modifier oathExists(uint _oathId){
+        require(oaths[_oathId].timeCreated != 0, "Oath does not exist");
         _;
     }
-    
-    modifier milestoneExists(){
+
+    modifier milestoneExists(uint _oathId, uint _milestoneId){
+        require(milestones[_oathId][_milestoneId].milestoneCreated != 0, "Milestone does not exist");
         _;
     }
-    
-    modifier senderHasPendingWithdrawals(){
+
+    modifier senderHasPendingWithdrawals(address _recipient){
+        require(pendingWithdrawals[_recipient] != 0, "Recipient has no pending withdrawals");
         _;
     }
-    
-    
-    
+
+
+
     /**
      * EVENTS
     */
     event oathCreated(uint indexed oathid);
-    
+
     event milestoneCreatedForOath(uint indexed oathId);
-    
+
     event oathGiverMarkMilestone(address oathGiver, uint indexed oathid, uint indexed milestoneId);
-    
+
     event oathTakerMarkMilestone(address oathTaker, uint indexed oathid, uint indexed milestoneId);
-    
+
     event withdrawalMade(address indexed recipient, uint indexed amount);
-    
+
     event confirmationsComplete(uint indexed milestoneId);
-    
+
     event recipientCredited(address recipient, uint amount);
-    
-    
+
+
     /// @notice create oath
     /// @param _deadline When oath expires
     /// @param _oathTaker (required) user who can mark milestone as done
     /// @param _defaultsRecipient (required) address to pay when milestone is missed
     /// @param _completionRecipient (required) address to pay when milestone is crushed
     /// @param _body some description of the milestone
-    /// @return _oathId 
+    /// @return _oathId
     function createOath(
-        uint _deadline, 
-        address payable _oathTaker, 
+        uint _deadline,
+        address payable _oathTaker,
         address payable _defaultsRecipient,
         address payable _completionRecipient,
         string memory _body
-    ) 
+    )
     public returns (uint _oathId)
     {
         _oathId = oathCount++;
         Oath memory newoath = Oath(
-            _oathId, 
-            _body, 
-            block.timestamp, 
+            _oathId,
+            _body,
+            block.timestamp,
             block.timestamp + _deadline,
             0,
-            msg.sender, 
-            _oathTaker, 
-            _defaultsRecipient, 
-            _completionRecipient, 
+            msg.sender,
+            _oathTaker,
+            _defaultsRecipient,
+            _completionRecipient,
             false
         );
         oaths[_oathId] = newoath;
         emit oathCreated(_oathId);
     }
-    
-    /// @notice 
-    /// @dev 
-    /// @return oathCount 
+
+    /// @notice
+    /// @dev
+    /// @return oathCount
     function getOathCount() public view returns (uint){
         return oathCount;
     }
-    
+
     /// @notice Add milestone to existing oath
     /// @param _oathId The id of oath to add milestone
-    /// @return updatedOathId 
+    /// @return updatedOathId
     function addMilestoneToOath(
         uint _oathId,
         string memory _milestoneBody,
         uint8 _confirmations,
         uint _milestoneValue,
         uint _milestoneDeadline
-    ) 
-    public payable returns (uint)
+    )
+    public payable
+    oathExists(_oathId)
+    onlyOathGiver(msg.sender, _oathId) returns (uint)
     {
         Milestone memory milestone = Milestone(
-            _milestoneBody, 
+            _milestoneBody,
             _confirmations,
             0,
-            _milestoneValue, 
-            block.timestamp, 
-            block.timestamp + _milestoneDeadline, 
-            false, 
+            _milestoneValue,
+            block.timestamp,
+            block.timestamp + _milestoneDeadline,
+            false,
             false,
             false
         );
@@ -152,13 +160,17 @@ contract OathKeeper{
         emit milestoneCreatedForOath(_oathId);
         return _oathId;
     }
-    
+
     /// @notice
-    /// @dev 
+    /// @dev
     /// @param _oathId The id of oath to add milestone
     /// @return _oathId, _milestoneId
-    function oathGiverMarkMilestoneAsDone(uint _oathId, uint _milestoneId) public
-        returns (uint, uint) 
+    function oathGiverMarkMilestoneAsDone(uint _oathId, uint _milestoneId)
+    public
+    oathExists(_oathId)
+    milestoneExists(_oathId, _milestoneId)
+    onlyOathGiver(msg.sender, _oathId)
+    returns (uint, uint)
     {
         // Milestone memory milestone = milestones[_oathId][_milestoneId];
         milestones[_oathId][_milestoneId].confirmationsSoFar++;
@@ -173,13 +185,17 @@ contract OathKeeper{
         emit oathGiverMarkMilestone(msg.sender, _oathId, _milestoneId);
         return(_oathId, _milestoneId);
     }
-    
-    /// @notice 
-    /// @dev 
+
+    /// @notice
+    /// @dev
     /// @param _oathId The id of oath to add milestone
     /// @return _oathId, _milestoneId
-    function oathTakerMarkMilestoneAsDone(uint _oathId, uint _milestoneId) public 
-        returns (uint, uint) 
+    function oathTakerMarkMilestoneAsDone(uint _oathId, uint _milestoneId)
+        public
+        oathExists(_oathId)
+        milestoneExists(_oathId, _milestoneId)
+        onlyOathTaker(msg.sender, _oathId)
+        returns (uint, uint)
     {
         Milestone storage milestone = milestones[_oathId][_milestoneId];
         milestone.confirmationsSoFar++;
@@ -194,26 +210,31 @@ contract OathKeeper{
         emit oathTakerMarkMilestone(msg.sender, _oathId, _milestoneId);
         return(_oathId, _milestoneId);
     }
-    
-    /// @notice 
-    /// @dev 
-    /// @return 
-    function withdrawFunds() public {
+
+    /// @notice
+    /// @dev
+    /// @return amount The ether value sent to recipient
+    function withdrawFunds()
+        public
+        senderHasPendingWithdrawals(msg.sender)
+        returns (uint)
+    {
         uint amount = pendingWithdrawals[msg.sender];
         // Avoid re-entrancy by zeroing amount before sending;
         pendingWithdrawals[msg.sender] = 0;
         msg.sender.transfer(amount);
         emit withdrawalMade(msg.sender, amount);
+        return amount;
     }
-    
+
     /// @notice Fallback function
-    function() external payable{
+    fallback() external payable{
         return;
     }
-    
+
     // function oathVerifierMarkMilestoneAsDone(uint _oathId) public returns (uint) {
     //     milestones[_oathId].confirmations++;
     //     milestones[_oathId].oathGiverVerified = true;
     // }
- 
+
 }
